c0c35eb2eb52dfcea26c31aa1b0557953ca3e30e
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-10-25 15:11:00 +0200

    first commit

---
+++ mail/js/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+// Consegna es. Mail:
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
